bc45640f61081400e4222350a6160984016f9cb0
Caleb <dougalcaleb@gmail.com>
2021-08-15 15:44:49 -0600

    first commit

---
+++ bot.js
@@ -0,0 +1,12 @@
+const Net = require("net");
+const {Client, Intents} = require("discord.js");
+const Filesystem = require("fs");
+const config = JSON.parse(Filesystem.readFileSync("./config.json", "utf-8"));
+
+
+class Utility {
+   constructor() { }
+   
+}
+
+const Util = new Utility();
